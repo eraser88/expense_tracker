450a2fa660b4519160b9630260ec7c61bc9af5e0
priya <priya.krish.6@gmail.com>
2014-07-15 19:54:53 +0530

    Added edit and delete features

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,31 @@
 class ExpensesController < ApplicationController
   def new
   end
+
   def create
-    @expense = Expense.new(params.require(:expense).permit(:date,:category,:amount))
+    @expense = Expense.new(params.require(:expense).permit(:date, :category, :amount))
     @expense.save
+    redirect_to dashboard_index_path
+  end
+
+  def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+
+    if @expense.update(params.require(:expense).permit(:date, :category, :amount))
+      redirect_to dashboard_index_path
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+    @expense.destroy
     redirect_to dashboard_index_path
   end
 end
